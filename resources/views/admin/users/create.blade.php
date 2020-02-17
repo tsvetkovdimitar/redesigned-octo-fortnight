5c944730a378f5b1a43ceeec875d8b5c3cb38876
Dimitar Tsvetkov <tsvetkovdimitar@gmail.com>
2020-02-17 21:34:01 +0000

    Password field and custom request

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -29,6 +29,11 @@
     </div>
 
     <div class="form-group">
+        {!! Form::label('password', 'Password:') !!}
+        {!! Form::password('password', ['class'=>'form-control']) !!}
+    </div>
+
+    <div class="form-group">
         {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
     </div>
 
