5fc135ed22e9880b72998dacb1b386d72d547cbd
Dimitar Tsvetkov <tsvetkovdimitar@gmail.com>
2020-03-15 11:24:41 +0000

    updating users last touch

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersEditRequest;
 use App\Http\Requests\UsersRequest;
 use App\Photo;
 use App\Role;
@@ -50,7 +51,19 @@ class AdminUsersController extends Controller
 //        return $request->all();
 //        User::create($request->all());
 
-        $input = $request->all();
+        if(trim($request->password) == ''){
+
+            $input = $request->except('password');
+
+        }else{
+
+            $input = $request->all();
+            $input['password'] = bcrypt($request->password);
+
+        }
+
+
+//        $input = $request->all();
 
         if($file = $request->file('photo_id')){
 
@@ -64,7 +77,7 @@ class AdminUsersController extends Controller
 
         }
 
-        $input['password'] = bcrypt($request->password);
+//        $input['password'] = bcrypt($request->password);
 
         User::create($input);
 
@@ -109,9 +122,45 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UsersEditRequest $request, $id)
     {
         //
+        $user = User::findOrFail($id);
+
+//        $input = $request->all();
+
+//        var_dump($input); exit;
+
+        if(trim($request->password) == ''){
+
+            $input = $request->except('password');
+
+        }else{
+
+            $input = $request->all();
+            $input['password'] = bcrypt($request->password);
+
+        }
+
+        if($file = $request->file('photo_id')){
+
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file' => $name]);
+
+            $input['photo_id'] = $photo->id;
+
+        }
+
+//        if (is_null($input['password'])) unset($input['password']);
+
+        $user->update($input);
+
+        return redirect('admin/users');
+
+//        return $request->all();
     }
 
     /**
