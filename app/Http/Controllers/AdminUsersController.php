7ba1547caa2a8f0ca68640103527140878e913c5
Dimitar Tsvetkov <tsvetkovdimitar@gmail.com>
2020-03-23 22:47:44 +0000

    Delete user fucntionality added

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -172,5 +172,9 @@ class AdminUsersController extends Controller
     public function destroy($id)
     {
         //
+        User::findOrFail($id)->delete();
+
+        return redirect('/admin/users');
+
     }
 }
