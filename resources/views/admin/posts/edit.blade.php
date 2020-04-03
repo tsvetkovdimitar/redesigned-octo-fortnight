8dbcce1e9d3db3be483d612a3bd3bc3453d64b13
Dimitar Tsvetkov <tsvetkovdimitar@gmail.com>
2020-04-03 16:13:08 +0100

    Added post image to Edit Post screen

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -11,39 +11,53 @@
 
     <div>
 
-        {!! Form::model($post, ['method'=>'PATCH', 'action'=>['AdminPostsController@update', $post->id], 'files'=>true]) !!}
+        <div class="col-sm-3">
 
-        <div class="form-group">
-            {!! Form::label('title', 'Title') !!}
-            {!! Form::text('title', null, ['class'=>'form-control']) !!}
-        </div>
-        <div class="form-group">
-            {!! Form::label('category_id', 'Category') !!}
-            {!! Form::select('category_id', $categories, null, ['class'=>'form-control']) !!}
-        </div>
-        <div class="form-group">
-            {!! Form::label('photo_id', 'Photo') !!}
-            {!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
-        </div>
-        <div class="form-group">
-            {!! Form::label('body', 'Description') !!}
-            {!! Form::textarea('body', null, ['class'=>'form-control']) !!}
+            @if ($post->photo()->exists())
+                <td><img src="{!! asset($post->photo->file)!!}" alt="" class="img-responsive img-rounded"></td>
+            @else
+                <td><img src="{!! asset('images/placeholder.png')!!}" alt="" class="img-responsive img-rounded"></td>
+            @endif
+
         </div>
 
-        <div class="form-group">
-            {!! Form::submit('Update Post', ['class'=>'btn btn-primary col-sm-6']) !!}
-        </div>
+        <div class="col-sm-9">
 
-        <div>
-            {!! Form::open(['method'=>'DELETE', 'action'=>['AdminPostsController@destroy', $post->id]]) !!}
+            {!! Form::model($post, ['method'=>'PATCH', 'action'=>['AdminPostsController@update', $post->id], 'files'=>true]) !!}
 
             <div class="form-group">
-                {!! Form::submit('Delete Post', ['class'=>'btn btn-danger col-sm-6']) !!}
+                {!! Form::label('title', 'Title') !!}
+                {!! Form::text('title', null, ['class'=>'form-control']) !!}
             </div>
+            <div class="form-group">
+                {!! Form::label('category_id', 'Category') !!}
+                {!! Form::select('category_id', $categories, null, ['class'=>'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('photo_id', 'Photo') !!}
+                {!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('body', 'Description') !!}
+                {!! Form::textarea('body', null, ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::submit('Update Post', ['class'=>'btn btn-primary col-sm-6']) !!}
+            </div>
+
+            <div>
+                {!! Form::open(['method'=>'DELETE', 'action'=>['AdminPostsController@destroy', $post->id]]) !!}
+
+                <div class="form-group">
+                    {!! Form::submit('Delete Post', ['class'=>'btn btn-danger col-sm-6']) !!}
+                </div>
+            </div>
+
+            {!! Form::close() !!}
+
+
         </div>
-
-        {!! Form::close() !!}
-
 
     </div>
 
