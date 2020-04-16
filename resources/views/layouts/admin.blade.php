598b7ab941f4f084ca8dd9871fab60cd125f8f23
Dimitar Tsvetkov <tsvetkovdimitar@gmail.com>
2020-04-16 18:11:40 +0100

    Admin panel and post layouts fixed

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -12,7 +12,7 @@
     <title>Admin</title>
 
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+{{--    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">--}}
 
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
@@ -37,7 +37,7 @@
 
 
     <!-- Bootstrap Core CSS -->
-    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+{{--    <link href="{{asset('css/app.css')}}" rel="stylesheet">--}}
 
     <link href="{{asset('css/libs.css')}}" rel="stylesheet">
 
@@ -53,7 +53,7 @@
 
 </head>
 
-<body id="admin-page">
+<body id="admin-page" style="padding-top: 0">
 
 <div id="wrapper">
 
--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,16 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/admin', function(){
+Route::get('/post/{id}', ['as'=>'home.post', 'uses'=>'AdminPostsController@post']);
 
-    return view('admin.index');
-
-});
 
 Route::group(['middleware'=>'admin'], function(){
+
+    Route::get('/admin', function(){
+
+        return view('admin.index');
+
+    });
 
     Route::resource('admin/users', 'AdminUsersController');
 
