cda3133d51827a064dff6947401c749f26010191
Dimitar Tsvetkov <tsvetkovdimitar@gmail.com>
2020-04-17 15:13:40 +0100

    Displaying Posts Comments

--- resources/views/admin/comments/index.blade.php
+++ resources/views/admin/comments/index.blade.php
@@ -7,7 +7,42 @@
 @section('content')
 
 
-    <h1>Comments</h1>
 
+
+    @if(count($comments) > 0)
+
+        <h1>Comments</h1>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Author</th>
+                <th>Email</th>
+                <th>Body</th>
+            </tr>
+        </thead>
+        <tbody>
+
+        @foreach($comments as $comment)
+
+            <tr>
+                <td>{{$comment->id}}</td>
+                <td>{{$comment->author}}</td>
+                <td>{{$comment->email}}</td>
+                <td>{{$comment->body}}</td>
+                <td><a href="{{route('home.post', $comment->post->id)}}">View Post</a></td>
+            </tr>
+
+            @endforeach
+
+        </tbody>
+    </table>
+
+        @else
+
+        <h1 class="text-center">No Comments</h1>
+
+    @endif
 
 @stop
