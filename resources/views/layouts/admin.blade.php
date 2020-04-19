f8b75a8c80240154aaf9f13304d679a4565e6270
Dimitar Tsvetkov <tsvetkovdimitar@gmail.com>
2020-04-19 08:32:26 +0100

    Creating and displaying replies

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -37,9 +37,9 @@
 
 
     <!-- Bootstrap Core CSS -->
-{{--    <link href="{{asset('css/app.css')}}" rel="stylesheet">--}}
-
     <link href="{{asset('css/libs.css')}}" rel="stylesheet">
+
+{{--    <link href="{{asset('css/app.css')}}" rel="stylesheet">--}}
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -81,20 +81,27 @@
             </h4>
             <p>{{$comment->body}}</p>
 
+        @if(count($comment->replies) > 0)
+
+            @foreach($comment->replies as $reply)
+
             <!-- Nested Comment -->
-            <div class="media">
+            <div class="media" style="padding-top: 10px">
                 <a class="pull-left" href="#">
-                    <img class="media-object" src="http://placehold.it/64x64" alt="">
+                    <img height="64" class="media-object" src="{!! asset($reply->photo)!!}" alt="">
                 </a>
                 <div class="media-body">
-                    <h4 class="media-heading">Nested Start Bootstrap
-                        <small>August 25, 2014 at 9:30 PM</small>
+                    <h4 class="media-heading">{{$reply->author}}
+                        <small>{{$reply->created_at->diffForHumans()}}</small>
                     </h4>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                   <p>{{$reply->body}}</p>
                 </div>
 
                 {!! Form::open(['method'=>'POST', 'action'=>'CommentRepliesController@createReply']) !!}
                     <div class="form-group">
+
+                        <input type="hidden" name="comment_id" value="{{$comment->id}}">
+
                         {!! Form::label('body', 'Body') !!}
                         {!! Form::textarea('body', null, ['class'=>'form-control','rows'=>1]) !!}
                     </div>
@@ -107,6 +114,10 @@
 
             </div>
             <!-- End Nested Comment -->
+
+                @endforeach
+
+            @endif
 
         </div>
     </div>
